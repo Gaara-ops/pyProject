77a3606fa9f3fee79b65d91ed31dca8d14da6276
yuxing555 <2219502781@qq.com>
2018-04-27 13:39:01 +0800

    use git api create chart

--- use_api/python_repos.py
+++ use_api/python_repos.py
@@ -1,4 +1,7 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
+
 
 #执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -8,7 +11,29 @@ print("Status code:",r.status_code)
 response_dict = r.json()
 #搜索有关仓库的信息
 repo_dicts = response_dict['items']
-
-repo_dict = repo_dicts[0]
-for key in sorted(repo_dict.keys()):
-    print(key)
+names,plot_dicts = [],[]
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    plot_dict = {
+        'value':repo_dict['stargazers_count'],
+        'label':repo_dict['full_name'],
+        'xlink':repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
+#可视化
+my_style = LS('#333366',base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config,style=my_style)
+chart.title = 'Most-StarredPythonProjects on GitHub'
+chart.x_labels = names
+chart.add('',plot_dicts)
+chart.render_to_file('python_repos.svg')
+url = 'https://hacker-news.firebaseio.com/v0/item/9884165.json'
