d64ce2cdbea17509e18c896eb74a3dcd0ef42351
yuxing555 <2219502781@qq.com>
2018-05-29 09:45:57 +0800

    方差和协方差求解,α和β的一元线性回归模型求解

--- intelligence/linear_regression.py
+++ intelligence/linear_regression.py
@@ -13,17 +13,26 @@ def runplt():
     return plt
 
 plt = runplt()
-X = [[6],[8],[10],[15],[18]]
+X = [[6],[8],[10],[14],[18]]
 y = [[7],[9],[13],[17.5],[18]]
 plt.plot(X,y,'k.')
 # ~ plt.show()
 X2 = [[0],[10],[14],[25]]
 #create and predict
 model = LinearRegression()
+#LinearRegression的fit()方法学习一元线性回归模型：y = α + βx
+# β=协方差/方差    α = y平均 - βx平均
 model.fit(X,y)
 y2 = model.predict(X2)
 #残差平方和
 print('residual sum of squares:%.2f' % np.mean((model.predict(X)-y)**2))
+xbar = (6+8+10+14+18)/5
+ybar = (7+9+13+17.5+18)/5
+#求x的方差
+variance = ((6-xbar)**2+(8-xbar)**2+(10-xbar)**2+(14-xbar)**2+(18-xbar)**2)/4
+print(np.var([6,8,10,14,18],ddof=1))
+#求x,y的协方差
+print(np.cov([6,8,10,14,18],[7,9,13,17.5,18])[0][1])
 # ~ y3 = [14.25,14.25,14.25,14.25]
 # ~ y4 = y2 * 0.5 + 5
 # ~ model.fit(X[1:-1],y[1:-1])
