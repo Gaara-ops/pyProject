f51ee8bbce22a41198841b50d3ec907aee9d8dda
yuxing555 <2219502781@qq.com>
2018-12-06 09:40:46 +0800

    analysis log with threadid

--- LogAnalysis/logAnalysis.py
+++ LogAnalysis/logAnalysis.py
@@ -3,143 +3,119 @@ import numpy
 import string
 import datetime
 import time
-#i want to create a tag
+
 flag1 = 'start'
 flag2 = 'finish'
 timelength = 24
-threadlength = 30
-StartDict = {}
-FinishDict = {}
-
-strTime1 = ''
-strTime2 = ''
-strName1 = ''
-strName2 = ''
-lastTime = ''
-lastName = ''
+threadStartIndex = 29
+DictName = {}
+DictTime = {}
 timeFormat = '%Y-%m-%d %H:%M:%S:%f'#时间格式
 allSaveData = ''
-timeSpace = 0
-timeUse = 0;
-inVA = 0;
-#其他判断
-strTimeOther1 = ''
-strTimeOther2 = ''
-strCaSoring = "CalcificationScoreCalculation"
-strStraighten = "GenerateStraightenVolume"
-strAreaCurve = "ComputeAreaCurve"
-def GetOtherFlagInfo(line):
-    global allSaveData,strTimeOther1,strTimeOther2
-    if(inVA == 0):
-        return 0,'-1','-1'
-    str1 = strCaSoring + " Begin"
-    str2 = strCaSoring + " End"
-    str3 = strStraighten + " Begin"
-    str4 = strStraighten + " End"
-    str5 = strAreaCurve + " Begin"
-    str6 = strAreaCurve + " End"
-    isstr = "";
-    strtype = 1;
-    if(str1 in line):
-        isstr = str1
-        strtype = 1
-    elif(str2 in line):
-        isstr = str2
-        strtype = 2
-    elif(str3 in line):
-        isstr = str3
-        strtype = 1
-    elif(str4 in line):
-        isstr = str4
-        strtype = 2
-    elif(str5 in line):
-        isstr = str5
-        strtype = 1
-    elif(str6 in line):
-        isstr = str6
-        strtype = 2
-    if(isstr == ""):
-        return 0,'-1','-1'
-    strTime = line[1:timelength]
-    allSaveData += strTime + '--' + isstr+ '\n'
-    if(strtype == 1):
-        strTimeOther1 = strTime
-    elif(strtype == 2):
-        strTimeOther2 = strTime
-        time1  = datetime.datetime.strptime(strTimeOther1,timeFormat)
-        time2  = datetime.datetime.strptime(strTimeOther2,timeFormat)
-        timeInterval = (time2-time1).total_seconds()
-        print ('************',isstr,'--',timeInterval,'\n')
-    
+DictTimeUse = {}
+DictTimeSpace = {}
+allTimeData = ''
+allTimeList = []
 
-def SaveInfo(line,flag,strTime,strName):
-    global allSaveData,StartDict,FinishDict#声明为全局变量
-    if(flag in line):#判断字符串line是否包含字符串flag
-        startIndex = line.find(flag)#查找flag在line中位置
-        interfaceStr = line[startIndex:]#获取从某一位置到结束的字符串
-        strTime = line[1:timelength]#获取从1到x处的字符串
-        allSaveData += strTime + '--' + interfaceStr#字符串拼接
-        ifArr = interfaceStr.split('|')#字符串分割
-        if(len(ifArr) < 3):#判断数组的长度
-            return 0,'-1','-1'
-        lastIFName = ifArr[-2]#获取数组中倒数第二个元素
-        strName = lastIFName.strip()#去除空格
-        return 1,strTime,strName#多个返回参数
-    return 0,'-2','-2'
+def SaveInfo(line,flag):
+    global allSaveData
+    if(flag in line):
+        #find thread id
+        strFindThread = line[threadStartIndex:]
+        threadEndIndex = strFindThread.find(']')
+        resThreadID = strFindThread[0:threadEndIndex]
+        
+        startIndex = line.find(flag)
+        interfaceStr = line[startIndex:]#get all interface name
+        resStrTime = line[1:timelength]#get time
+        allSaveData += resStrTime+'--'+resThreadID+'--' + interfaceStr
+        
+        ifArr = interfaceStr.split('|')#判断命令是否有效
+        if(len(ifArr) < 3):
+            return 0,'-1','-1','-1'
+        lastIFName = ifArr[-2]#get last name
+        resStrName = lastIFName.strip()#去除空格
+        return 1,resStrTime,resStrName,resThreadID#多个返回参数
+    return 0,'-1','-1','-1'
+
+def GetTimeInterval(strtime1,strtime2):
+    time1  = datetime.datetime.strptime(strtime1,timeFormat)
+    time2  = datetime.datetime.strptime(strtime2,timeFormat)
+    timeInterval = (time2-time1).total_seconds()
+    return timeInterval
+
+def GetInterfaceTime(threadid,finishtime):
+    if threadid in DictName:
+        startname = DictName.get(threadid)
+        starttime = DictTime.get(startname)
+        time1  = datetime.datetime.strptime(starttime,timeFormat)
+        time2  = datetime.datetime.strptime(finishtime,timeFormat)
+        timeInterval = (time2-time1).total_seconds()
+        return timeInterval
+    return 0
+
+def DelLastItem(threadid):
+    if threadid in DictName:
+        tempname = DictName.get(threadid)
+        del DictName[threadid]
+        del DictTime[tempname]
+
+def UpdateTimeUse(threadid,timeuse):
+    global DictTimeUse,DictTimeSpace
+    alltimeuse = 0
+    if threadid in DictTimeUse:
+        alltimeuse = DictTimeUse.get(threadid)
+    DictTimeUse[threadid] = timeuse+alltimeuse
+
+def UpdateTimeSpace(threadid,timespace):
+    global DictTimeUse,DictTimeSpace
+    alltimespace = 0
+    if threadid in DictTimeSpace:
+        alltimespace = DictTimeSpace.get(threadid)
+    DictTimeSpace[threadid] = timespace+alltimespace
+
+def CalculateAllTime(strtime,threadid,strname):
+    global allTimeData,allTimeList
+    if(len(allTimeList)>0):
+        timeInterval = GetTimeInterval(allTimeList[-1],strtime)
+        if(timeInterval>=0):
+            allTimeList.append(strtime)
+            allTimeData += strtime+'--'+threadid+'--'+strname+'\n'
+    else:
+        allTimeList.append(strtime)
+        allTimeData += strtime+'--'+threadid+'--'+strname+'\n'
 
 def LoadData(strfile):
-    global allSaveData,strName1,strName2,strTime1,strTime2
-    global lastTime,lastName,timeSpace,timeUse
-    global StartDict,FinishDict
-    global inVA,strTimeOther1,strTimeOther2
+    global allSaveData,DictTimeUse,DictTimeSpace
     lines=[]
-    with open(strfile) as pFile:#打开文件
-        """
-        读取所有文件,注意读取后文件指针偏移至最后
-        allData = (pFile.read())
-        """
-        lines = pFile.readlines()#读取所有行
-    #逐行读取文件
+    with open(strfile) as pFile:
+        lines = pFile.readlines()
     for line in lines:
-        res1,strTime,strName = SaveInfo(line,flag1,strTime1,strName1)
+        res1,strTime,strName,threadID = SaveInfo(line,flag1)
         if(res1 == 1):
-            strTime1 = strTime
-            strName1 = strName
-        if(res1 == 1 and strName1 == "StartVesselAnalysis"):
-            strTimeOther1 = ''
-            strTimeOther2 = ''
-            inVA = 1
-        res2,strTime,strName = SaveInfo(line,flag2,strTime2,strName2)
+            CalculateAllTime(strTime,threadID,strName)
+            timespace = GetInterfaceTime(threadID,strTime)
+            UpdateTimeSpace(threadID,timespace)
+            if(timespace > 0.1):
+                print ('\ntimespace-->',threadID,'--',strName,'--',timespace,'\n')
+            DelLastItem(threadID)
+            DictName[threadID] = strName
+            DictTime[strName] = strTime
+            
+        res2,strTime,strName,threadID = SaveInfo(line,flag2)
         if(res2 == 1):
-            strTime2 = strTime
-            strName2 = strName
-        if(res2 == 1 and strName2 == "StartVesselAnalysis"):
-            strTimeOther1 = ''
-            strTimeOther2 = ''
-            inVA = 0
-        
-        GetOtherFlagInfo(line)
-        
-        if(strTime1!='' and lastTime!='' and res1):
-            time1  = datetime.datetime.strptime(lastTime,timeFormat)
-            time2  = datetime.datetime.strptime(strTime1,timeFormat)
-            timeInterval = (time2-time1).total_seconds()
-            timeSpace += timeInterval
-            if timeInterval>0.1:
-                print ('\n',lastName,'->',strName1,'--',timeInterval,'\n')
-        if((strName1 == strName2) and strName1!=''):
-            #字符串转datetime
-            time1  = datetime.datetime.strptime(strTime1,timeFormat)
-            time2  = datetime.datetime.strptime(strTime2,timeFormat)
-            #datetime时间间隔计算
-            timeInterval = (time2-time1).total_seconds()
-            timeUse += timeInterval
-            if timeInterval>0.1:
-                print (strName1,'--',timeInterval)
-            allSaveData += strName1 + '--' + str(timeInterval) + '\n'
-            lastTime = strTime2
-            lastName = strName2
-            strName1 = strName2 = ''
+            CalculateAllTime(strTime,threadID,strName)
+            timeuse = GetInterfaceTime(threadID,strTime)
+            UpdateTimeUse(threadID,timeuse)
+            if(timeuse > 0.1):
+                print (strName,'--',timeuse)
+            allSaveData += strName + '--' + str(timeuse) + '\n'
+            del DictName[threadID]
+            del DictTime[strName]
+            DictName[threadID] = strName
+            DictTime[strName] = strTime
+            
+    
             
 logpath = 'G:/iMAGESServer/branches/Version2/Bin/Win32/Server/MinGW/Debug/SpiderSightServer/Log/log.log'
 LoadData(logpath)
@@ -148,9 +124,10 @@ time_str = datetime.datetime.strftime(now_time,'%Y%m%d_%H_%M_%S')
 outfilepath = time_str + '_logAnalysis.txt'
 outfilepath = 'test.txt'
 with open(outfilepath,'w') as saveFile:#写入数据到文件
-    saveFile.write(allSaveData)
+    saveFile.write(allSaveData+'\n\n'+allTimeData)
 print ('end')
-print ('allTime:',timeUse+timeSpace)
-print ('timeUse:',timeUse)
-print ('timeSpace:',timeSpace)
+print ('DictTimeUse:',DictTimeUse)
+print ('DictTimeSpace:',DictTimeSpace)
+print ('all time use:',GetTimeInterval(allTimeList[0],allTimeList[-1]))
+
 pausett = input('pause')
